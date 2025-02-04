73d5962888812440ce3d90eb2a24484b03c6d64a
martin <martin@xyzcorp.io>
2025-02-04 14:13:05 +0900

    일회성 GUI 성공

--- src/gui_launcher/gui_launcher/gui_launch.py
+++ src/gui_launcher/gui_launcher/gui_launch.py
@@ -9,14 +9,15 @@ import sys
 from gui_launcher.robot_client import RobotServiceClient
 from robot_gui.gui_set.gui_control import MyApp
 
-class GUIlaunch(Node):
-    def __init__(self, client):
-        super().__init__('gui_launch')
+class GUIlaunch():
+    def __init__(self):
+        # super().__init__('gui_launch')
         self.gui = None
         self.app = None
         self.service_client = None
         self.queue_thread = None
-        self.robot_client = client
+        self.robot_client = RobotServiceClient()
+        self.executor = MultiThreadedExecutor()
     def launch(self):
         
         # GUI 실행을 메인 스레드에서 시작
@@ -24,39 +25,46 @@ class GUIlaunch(Node):
         self.gui = MyApp()
         self.gui.show()
 
+        self.executor.add_node(self.robot_client)
+
         # 명령을 처리하는 스레드 시작
         self.queue_thread = threading.Timer(interval=0.1, function=self.process_commands)
         self.queue_thread.start()
-        # self.executor_thread = threading.Thread(target=self.executor.spin)
-        # self.executor_thread.start()
+        
+        self.executor_thread = threading.Thread(target=self.executor.spin)
+        self.executor_thread.start()
+        
         sys.exit(self.app.exec())
-            
         # finally:
         #     # self.executor.shutdown()
         #     # self.queue_thread.join()
         #     # self.executor_thread.join()
-            
+        
     def process_commands(self):
         while True:
             if self.gui.cmd_list:
                 command = self.gui.cmd_list.popleft()
                 print(f"Input Command Cmd: {command['request_id'], command['no']}")
                 self.robot_client.get_command(command)
-            time.sleep(0.1)  # CPU 사용을 방지하기 위해 약간의 대기시간 추가
+            else :
+                print("GUI Queue is Empty")
+            time.sleep(0.5)  # CPU 사용을 방지하기 위해 약간의 대기시간 추가
         
 
 def main(args=None):
     rclpy.init(args=args)
-    robot_client = RobotServiceClient()
-    gui = GUIlaunch(robot_client)
-    executor = MultiThreadedExecutor()
-    executor.add_node(gui)
-    executor.add_node(robot_client)
-    try:
-        executor.spin()
-    except Exception as error:
-        executor.shutdown()
-        gui.queue_thread.join()
+    # robot_client = RobotServiceClient()
+    gui = GUIlaunch()
+    gui.launch()
+    # executor = MultiThreadedExecutor()
+    # executor.add_node(gui)
+    # executor.add_node(gui.robot_client)
+    # try:
+    #     executor.spin()
+    # except Exception as error:
+    #     executor.remove_node(gui)
+    #     executor.shutdown()
+    #     gui.queue_thread.join()
         
     
 if __name__ == '__main__':
--- src/gui_launcher/gui_launcher/robot_client.py
+++ src/gui_launcher/gui_launcher/robot_client.py
@@ -1,45 +1,21 @@
 import rclpy as rp
-from robot_msg.srv import RobotService
+from message.srv import RobotService
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 import traceback
 import datetime
-import time
+import threading
 from gui_launcher.RobotRequestObject import RobotRequestObject
 from gui_launcher.DBConstants import DBFieldName
 
 class RobotServiceClient(Node):
-    HOLD_CUP = 'hold_cup'
-    UNHOLD_CUP = 'unhold_cup'
-    PLACE_COFFEE = 'place_coffee'
-    PICKUP_COFFEE = 'pickup_coffee'
-    UNHOLD_ICE = 'unhold_ice'
-    HOLD_ICE = 'hold_ice'
-    PICKUP_POWDER = 'pickup_powder'
-    PLACE_POWDER = 'place_powder'
-    PLACE_ORDER = 'place_order'
-    PICKUP_ORDER = 'pickup_order'
-    PLACE_HOLDER = 'place_holder'
-    PICKUP_HOLDER = 'pickup_holder'
-    GRIPPER_INIT = 'gripper_init'
-    SHAKE = 'shake'
-    HOME = 'home'
-    GREET = 'greet'
-    MOVE = 'move'
-    GESTURE = 'gesture'
-    PICKUP_LID = 'pickup_lid'
-    CLOSE_LID = 'close_lid'
-    PICKUP_SYRUP = 'pickup_syrup'
-    PLACE_SYRUP = 'place_syrup'
-    PUMP = 'pump'
-    UNHOLD_BOX = 'unhold_box'
-    HOLD_BOX = 'hold_box'
-    PLACE_BOX = 'place_box'
+    
     def __init__(self):
         super().__init__('RobotTestClient')
         self.qos_profile = QoSProfile(depth=25)
         self.client = self.create_client(RobotService, 'robot/service', qos_profile=self.qos_profile)
-        self.queue_timer = self.create_timer(timer_period_sec=0.5, callback=self.queue_checker)
+
+        self.queue_timer = threading.Timer(interval=1.0, function=self.queue_checker)
         self.pickup_index = 0
         self.robot_queue = []
 
@@ -68,25 +44,25 @@ class RobotServiceClient(Node):
         if len(self.robot_queue) != 0:
             srv_req = self.robot_queue.pop()
             print(f"Serivce Request : {srv_req.cmd}, {srv_req.rail_pos}, {srv_req.par1}")
-            response = self.send_request(srv_req)
-            print(f"Serivce Response : {response.seq_no}, {response.result}")
+            self.send_request(srv_req)
+            # print(f"Serivce Response : {response.seq_no}, {response.result}")
         else :
             print("No Input Data Found")
+
     def send_request(self, request):
         self.future = self.client.call_async(request)
-        rp.spin_until_future_complete(self, self.future)
-        return self.future.result()
+        rp.spin_until_future_complete(node = self, future=self.future, timeout_sec=0.5)
+        # return self.future.result()
+
+# def main(args=None):
+#     rp.init(args=args)
+#     robot_srv_client = RobotServiceClient()
+#     try:
+#         rp.spin(robot_srv_client)
+#     except Exception as ex:
+#         error_msg = traceback.format_exc(ex)
+#         robot_srv_client.get_logger().info(ex)
 
 
-def main(args=None):
-    rp.init(args=args)
-    robot_srv_client = RobotServiceClient()
-    try:
-        rp.spin(robot_srv_client)
-    except Exception as ex:
-        error_msg = traceback.format_exc(ex)
-        robot_srv_client.get_logger().info(ex)
-
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
